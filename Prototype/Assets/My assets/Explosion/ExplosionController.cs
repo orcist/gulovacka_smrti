d9195fefc27f42681650e1308ba70808d197edf9
Kubo Motyl <motyl.kubo@gmail.com>
2016-07-09 23:55:34 +0200

    add proof-of-concept prototype

---
+++ Prototype/Assets/My assets/Explosion/ExplosionController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ExplosionController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("die", 5);
+	}
+
+	private void die() {
+		Destroy(gameObject);
+	}
+}
---
+++ Prototype/Assets/My assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public int PlayerNumber;
+	public float Speed;
+	public GameObject ProjectileObject;
+	public float ProjectileSize;
+	public float FirePower;
+	public RectTransform Powerfill;
+
+	private Rigidbody2D rb;
+	private float power;
+	private Vector2 direction;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		power = 0;
+	}
+	void FixedUpdate() {
+		rb.velocity = new Vector2(
+				Input.GetAxis("Horizontal"+PlayerNumber),
+				-Input.GetAxis("Vertical"+PlayerNumber)
+			) * Speed;
+
+		if (rb.velocity.magnitude > 0)
+			direction = rb.velocity.normalized;
+
+		if (Input.GetButton("Fire"+PlayerNumber)) {
+			power = Mathf.Min(power + 0.025f, 1f);
+		}
+		else if (power > 0f) {
+			GameObject projectile = Instantiate(ProjectileObject) as GameObject;
+			projectile.transform.position = transform.position;
+			projectile.transform.localScale *= ProjectileSize;
+			projectile.GetComponent<Rigidbody2D>().AddForce(
+					direction * power * FirePower
+				);
+			power = 0f;
+		}
+
+		Powerfill.localScale = new Vector3(
+				Mathf.Lerp(Powerfill.localScale.x, power, Time.deltaTime * 5),
+				Powerfill.localScale.y,
+				Powerfill.localScale.z
+			);
+	}
+}
