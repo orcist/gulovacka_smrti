f2b27aaa887c9ae895fa9e6bfcfa983225901e41
Lucia Piváčková <pivackova.lucia@gmail.com>
2016-07-23 18:07:02 +0200

    add cheese damage

---
+++ Game/Assets/My assets/Level/CheeseController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CheeseController : MonoBehaviour {
+  public int HitPoints;
+
+  void OnCollisionEnter2D(Collision2D collision) {
+    // player resurrection
+  }
+
+  void OnCollisionStay2D(Collision2D collision) {
+    if (collision.gameObject.layer == LayerMask.NameToLayer("ObjectiveEnemy")) {
+      HitPoints--;
+      if (HitPoints <= 0) die();
+    }
+  }
+
+  private void die() {
+    Destroy(gameObject);
+  }
+}
