cb6a1c4d1f67e392c6329691611c78e389705c2d
Kubo Motyl <motyl.kubo@gmail.com>
2016-07-25 03:21:48 +0200

    mana regeneration

--- Game/Assets/My assets/Players/PlayerController.cs
+++ Game/Assets/My assets/Players/PlayerController.cs
@@ -5,49 +5,69 @@ public class PlayerController : MonoBehaviour {
 	public float Speed;
 	public GameObject ProjectileObject;
 	public float ProjectileSize;
+	public float ManaRegenRate;
 	public float FirePower;
-	public RectTransform Powerfill;
+	public RectTransform ManaBar, PowerFill;
   public bool Dead = false;
 
   private Rigidbody2D rb;
-	private float power;
+	private float mana = 0, power = 0;
 	private Vector2 direction;
 
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
-		power = 0;
 	}
 	void FixedUpdate() {
+		move();
+		regenMana();
+		handleFiring();
+	}
+	void Update() {
+		ManaBar.localScale = new Vector3(
+				Mathf.Lerp(ManaBar.localScale.x, mana, Time.deltaTime * 5),
+				ManaBar.localScale.y,
+				ManaBar.localScale.z
+			);
+		PowerFill.localScale = new Vector3(
+				Mathf.Lerp(PowerFill.localScale.x, power, Time.deltaTime * 5),
+				PowerFill.localScale.y,
+				PowerFill.localScale.z
+			);
+	}
+
+	private void move() {
 		Vector2 velocity = new Vector2(
 				Input.GetAxis("Horizontal"+PlayerNumber),
 				-Input.GetAxis("Vertical"+PlayerNumber)
 			) * Speed;
 
+		rb.velocity = velocity;
+
 		if (velocity.magnitude > 0)
 			direction = velocity.normalized;
+	}
 
+	private void regenMana() {
+		if (PlayerNumber == 1) {
+			if (rb.velocity.magnitude > 0)
+				mana = Mathf.Min(mana + (ManaRegenRate * rb.velocity.magnitude), 1f);
+		} else
+			mana = Mathf.Min(mana + ManaRegenRate, 1f);
+	}
+
+	private void handleFiring() {
 		if (Input.GetButton("Fire"+PlayerNumber)) {
-			power = Mathf.Min(power + 0.025f, 1f);
+			power = Mathf.Min(power + 0.025f, mana);
 		} else if (power > 0f) {
-			fire();
+			GameObject projectile = Instantiate(ProjectileObject, transform.position, Quaternion.identity) as GameObject;
+			projectile.layer = LayerMask.NameToLayer("ProjectileBy"+PlayerNumber);
+			projectile.transform.localScale *= ProjectileSize;
+			projectile.GetComponent<Rigidbody2D>().AddForce(
+					rb.velocity + direction * power * FirePower
+				);
+
+			mana-= power;
 			power = 0f;
 		}
-
-		rb.velocity = velocity;
-	}
-	void Update() {
-		Powerfill.localScale = new Vector3(
-				Mathf.Lerp(Powerfill.localScale.x, power, Time.deltaTime * 5),
-				Powerfill.localScale.y,
-				Powerfill.localScale.z
-			);
-	}
-	private void fire() {
-		GameObject projectile = Instantiate(ProjectileObject, transform.position, Quaternion.identity) as GameObject;
-		projectile.layer = LayerMask.NameToLayer("ProjectileBy"+PlayerNumber);
-		projectile.transform.localScale *= ProjectileSize;
-		projectile.GetComponent<Rigidbody2D>().AddForce(
-				rb.velocity + direction * power * FirePower
-			);
 	}
 }
